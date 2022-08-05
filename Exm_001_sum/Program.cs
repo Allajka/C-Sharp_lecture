2a3c336d9361cb32adb7d23db95e60addda6f57c
allajka <allajka94@gmail.com>
2022-08-05 15:59:51 +0300

    Exm_001_sum, Exm_003_likeName, Exm_004_Hello

--- Exm_001_sum/Program.cs
+++ Exm_001_sum/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int numberA = 2;
+int numberB = 4;
+int result = numberA + numberB;
+Console.Write("Сумма двух чисел:" + result);
+
---
+++ Exm_003_likeName/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша" || userName.ToLower() == "мария" )
+{
+    Console.WriteLine("Ура это Маша!");
+}
+else
+{
+    Console.WriteLine("Hello, " + userName + "!");
+}
