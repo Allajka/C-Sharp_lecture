cc5f5761548b47e8e5bfdc630ffef7bd3c24d0d8
allajka <allajka94@gmail.com>
2022-08-05 16:41:26 +0300

    Exm_009_MaxNumber, Exm_008_triangle

---
+++ Exm_009_MaxNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(0, 9);
+int b = new Random().Next(0, 9);
+int c = new Random().Next(0, 9);
+int d = new Random().Next(0, 9);
+int e = new Random().Next(0, 9);
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Максимальное число, среди чисел: " + a + ", "+ b + ", "+ c + ", "+ d + ", "+ e + " = " + max);
