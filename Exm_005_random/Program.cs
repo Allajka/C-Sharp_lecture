7eac4cc24135cc3328257193efc65be2dbb6358d
allajka <allajka94@gmail.com>
2022-08-05 16:08:12 +0300

    Exm_005_random, Exm_006_div

---
+++ Exm_005_random/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = new Random().Next(0, 9);
+int numberB = new Random().Next(0, 9);;
+int result = numberA + numberB;
+Console.Write("Сумма числа " + numberA + " и числа " + numberB + " равна: " + result);
---
+++ Exm_006_div/Program.cs
@@ -0,0 +1,4 @@
+﻿double numberA = 2;
+double numberB = 4;
+double result = numberA / numberB;
+Console.Write("Деление двух чисел:" + result);
