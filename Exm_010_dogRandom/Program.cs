70ab2be2a0590beb2e734ad5567a5515d9a0ce95
allajka <allajka94@gmail.com>
2022-08-06 12:48:43 +0300

    Exm_010_dogRandom, Exm_011_dogUser

---
+++ Exm_010_dogRandom/Program.cs
@@ -0,0 +1,31 @@
+﻿double firstFriendSpeed = new Random().Next(1, 3);
+double secondFriendSpeed = new Random().Next(2, 4);
+double dogSpeed = new Random().Next(3, 7);
+double distance = new Random().Next(10000, 17000);
+double distanceRuls = distance;
+double friend = 2;
+double index = 0;
+double time = 0;
+
+while (distance > 10)
+{
+    if (friend==1)
+        {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 2;
+        }
+    else
+        {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 1;
+        }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+    index = index + 1;
+}
+
+Console.WriteLine("Если:");
+Console.WriteLine($"- скорость первого друга: {firstFriendSpeed};");
+Console.WriteLine($"- скорость второго друга: {secondFriendSpeed};");
+Console.WriteLine($"- скорость собаки: {dogSpeed};");
+Console.WriteLine($"- дистанция: {distanceRuls};");
+Console.WriteLine($"Собака пробежит между друзьями {index} раз.");
---
+++ Exm_011_dogUser/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Программа позволяет решить, сколько раз пробежит собака между двумя друзьями.");
+Console.Write("Введите скорость первого друга: ");
+double firstFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость второго друга: ");
+double secondFriendSpeed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите собаки друга: ");
+double dogSpeed = Convert.ToInt32(Console.ReadLine());
+
+double distance = 10000;
+double friend = 2;
+double index = 0;
+double time = 0;
+
+while (distance > 10)
+{
+    if (friend==1)
+        {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 2;
+        }
+    else
+        {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 1;
+        }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+    index = index + 1;
+}
+
+Console.WriteLine($"Собака пробежит между друзьями {index} раз.");
